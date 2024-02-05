e8ffa12fe6b63b64ae694ffd1495fb388e213863
Stretch Spelman <codingspelman@gmail.com>
2024-02-04 17:12:03 -0700

    made thoughtController.js code

---
+++ controllers/thoughtController.js
@@ -0,0 +1,119 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  async getThoughts(req, res) {
+    try {
+      const dbThoughtData = await Thought.find();
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOne({
+        _id: req.params.thoughtId,
+      });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought does not exist" });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.username },
+        { $addToSet: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "Thought created, but no user found" });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        {
+          _id: req.params.thoughtId,
+        },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID does not exist" });
+      }
+      res.json({ message: "Updated Thought" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async deleteThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndRemove({
+        _id: req.params.thoughtId,
+      });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID does not exist" });
+      }
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: dbThoughtData.username },
+        { $pull: { thoughts: req.params.thoughtId } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "Thought deleted but no user with this ID" });
+      }
+      res.status(200).json({ message: "Thought Deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async addReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOne({
+        _id: req.params.thoughtId,
+      });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID does not exist" });
+      }
+      const dbUserData = await User.findOne({
+        _id: dbThoughtData.username,
+      });
+
+      dbThoughtData.reactions.push(req.body);
+      await dbThoughtData.save();
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+  async deleteReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID does not exist" });
+      }
+      res.json({ message: "Reaction deleted" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
